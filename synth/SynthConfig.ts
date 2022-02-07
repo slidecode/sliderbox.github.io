ea4bbe68137f93ab7992a1a0a7f2032558bcdfeb
slidecode <94769050+slidecode@users.noreply.github.com>
2022-02-07 12:41:08 -0500

    Update SynthConfig.ts

--- synth/SynthConfig.ts
+++ synth/SynthConfig.ts
@@ -238,8 +238,8 @@ export class Config {
 		{name: "B",  isWhiteKey:  true, basePitch: 23},
 	]);
 	public static readonly blackKeyNameParents: ReadonlyArray<number> = [-1, 1, -1, 1, -1, 1, -1, -1, 1, -1, 1, -1];
-	public static readonly tempoMin: number = 30;
-	public static readonly tempoMax: number = 300;
+	public static readonly tempoMin: number = 0;
+	public static readonly tempoMax: number = 3000;
 	public static readonly echoDelayRange: number = 24;
 	public static readonly echoDelayStepTicks: number = 4;
 	public static readonly echoSustainRange: number = 8;
@@ -362,7 +362,7 @@ export class Config {
 		{name: "arpeggio",        customInterval: false, arpeggiates:  true, strumParts: 0, singleTone:  true},
 		{name: "custom interval", customInterval:  true, arpeggiates: false, strumParts: 0, singleTone:  true},
 	]);
-	public static readonly maxChordSize: number = 4;
+	public static readonly maxChordSize: number = 32;
 	public static readonly operatorCount: number = 4;
 	public static readonly algorithms: DictionaryArray<Algorithm> = toNameMap([
 		{name: "1←(2 3 4)",   carrierCount: 1, associatedCarrier: [1, 1, 1, 1], modulatedBy: [[2, 3, 4], [],     [],  []]},
